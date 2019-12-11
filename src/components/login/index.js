947123eb0ae621315e097ad7b5e00dd484dbadbf
Denis <deeartrama88@gmail.com>
2019-12-11 20:51:31 +0200

    change eslint

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Modal from 'react-modal';
+import BasicForm from '../forms/basic';
 import './index.scss';
 
 const LoginMenu = () => {
+   const [isOpen, setIsOpen] = React.useState(false);
+   const toggleModal = val => setIsOpen(val ? val : !isOpen);
+   const customStyles = {
+      content: {
+         width: '50%',
+         height: '50%',
+         backgroundColor: 'rgba(0, 0, 0, 0.5)',
+         margin: 'auto'
+      }
+   };
    return (
       <div className="login">
          <FontAwesomeIcon icon="user" />
-         <span className="text">Login</span>
+         <span className="text" onClick={toggleModal}>
+            Login
+         </span>
+         <Modal
+            overlayClassName="modalDefaultOverlay"
+            style={customStyles}
+            isOpen={isOpen}
+         >
+            <BasicForm />
+         </Modal>
       </div>
    );
 };
